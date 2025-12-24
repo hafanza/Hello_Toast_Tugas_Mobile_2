9eecbe2e34d0023624941e44c9ca8b5860375065
hafanza <hafanzausten@gmail.com>
2025-12-25 03:18:52 +0700

    Update Sesuai Tugas 3 PAM

--- app/src/main/java/lat/pam/hellotoast/MainActivity.kt
+++ app/src/main/java/lat/pam/hellotoast/MainActivity.kt
@@ -14,16 +14,42 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     private var mCount = 0;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         val mShowCount = findViewById<TextView>(R.id.show_count)
         val buttonCountUp = findViewById<Button>(R.id.button_count)
         val buttonToast = findViewById<Button>(R.id.button_toast)
+        val buttonSwitchPage = findViewById<Button>(R.id.button_switchpage
+            val buttonBrowser = findViewById<Button>(R.id.button_browser)
+
+        )
 
 
         buttonCountUp.setOnClickListener(View.OnClickListener {
+            mCount++;
+            Log.d("mCount", Integer.toString(mCount))
+            if (mShowCount != null)
+                mShowCount.text = mCount.toString()
+        })
+
+
+        buttonToast.setOnClickListener(View.OnClickListener {
+            val tulisan: String = mShowCount?.text.toString()
+            val toast: Toast = Toast.makeText(this, "Angka yang dimunculkan "+tulisan, Toast.LENGTH_LONG)
+            toast.show()
+        })
+
+
+        buttonSwitchPage.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        })
+
+
+    buttonCountUp.setOnClickListener(View.OnClickListener {
             mCount++;
             if (mShowCount != null)
                 mShowCount.text = mCount.toString()
@@ -37,6 +63,17 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+            buttonBrowser.setOnClickListener(View.OnClickListener {
+                val intentbrowse = Intent(Intent.ACTION_VIEW)
+                intentbrowse.setData(Uri.parse("https://www.google.com/"))
+                startActivity(intentbrowse)
+            })
+
         }
     }
+}
+
+class R(activityMain: Any) {
+
 }
